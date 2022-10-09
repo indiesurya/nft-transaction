aab8f969dfcf0d0640ba70b90baf9e0f20bcd1ff
indiesurya <putuindie11@gmail.com>
2022-10-09 23:36:40 +0800

    Transaction send token

---
+++ app.js
@@ -0,0 +1,41 @@
+const { Api, JsonRpc, RpcError } = require('vexaniumjs');
+const { JsSignatureProvider } = require('vexaniumjs/dist/vexjs-jssig');
+
+const fetch = require('node-fetch'); // node only; not needed in browsers
+const { TextEncoder, TextDecoder } = require('util');  
+const rpc = new JsonRpc('https://explorer.vexanium.com:6960', {fetch});
+const schedule = require('node-schedule');
+
+const defaultPrivateKey = privateKey; // change to sender account private key
+const signatureProvider = new JsSignatureProvider([defaultPrivateKey]);
+const api = new Api({ rpc, signatureProvider, textDecoder: new 
+TextDecoder(), textEncoder: new TextEncoder() });
+
+
+function transfer(data){
+    api.transact({
+        actions: [{
+        account: data.smartContract,
+        name: 'transfer',
+        authorization: [{
+            actor: data.sender, //change to sender account
+            permission: 'active',
+        }],
+        data: {
+            from: data.sender, //change to sender account
+            to: data.receiver, //change to receiver account
+            quantity:data.quantity, //change to quantity token will be transferred
+            memo: data.memo// memo
+            },
+        }]
+    }, {
+        blocksBehind: 3,
+        expireSeconds: 30,
+    }).then((res) => {
+        console.log(res)
+    });
+}
+
+transfer(data);
+
+
